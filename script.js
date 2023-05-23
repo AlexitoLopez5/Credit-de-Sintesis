562b08982172938ef28e8c67ea9710b6f60fa759
AlexitoLopez5 <133109356+AlexitoLopez5@users.noreply.github.com>
2023-05-23 13:42:33 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function mostrarContenido(id) {
-    var contenidos = document.getElementsByClassName("contenido");
-    for (var i = 0; i < contenidos.length; i++) {
-      contenidos[i].style.display = "none";
-    }
-  
-    var contenido = document.getElementById(id);
-    contenido.style.display = "block";
-  }
